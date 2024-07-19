fc2189c2303796c4193171704b38b78dceafac09
khanglb2000 <lambaokhang2000@gmail.com>
2024-07-19 11:16:01 +0700

    update log

--- solutions/python-helloworld/app.py
+++ solutions/python-helloworld/app.py
@@ -1,8 +1,23 @@
 from flask import Flask
 from flask import json
 import logging
+import sys
 
 app = Flask(__name__)
+
+handler = logging.StreamHandler(sys.stdout)
+handler.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+
+error_handler = logging.StreamHandler(sys.stderr)
+error_handler.setLevel(logging.ERROR)
+error_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+error_handler.setFormatter(error_formatter)
+
+app.logger.addHandler(handler)
+app.logger.addHandler(error_handler)
+app.logger.setLevel(logging.DEBUG)
 
 @app.route('/status')
 def healthcheck():
@@ -11,7 +26,7 @@ def healthcheck():
             status=200,
             mimetype='application/json'
     )
-    app.logger.info('Status request successfull')
+    app.logger.info('Status request successful')
     app.logger.debug('DEBUG message')
     return response
 
@@ -22,17 +37,14 @@ def metrics():
             status=200,
             mimetype='application/json'
     )
-    app.logger.info('Metrics request successfull')
+    app.logger.info('Metrics request successful')
     return response
 
 @app.route("/")
 def hello():
-    app.logger.info('Main request successfull')
-
+    app.logger.info('Main request successful')
     return "Hello World!"
 
 if __name__ == "__main__":
-    ## stream logs to a file
-    logging.basicConfig(filename='app.log',level=logging.DEBUG)
-    
+    # Run the Flask application
     app.run(host='0.0.0.0')
